7089252c3aca4ec0d80fd9b4f819de047f870cef
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2020-04-28 11:16:10 +0200

    Utvecklad version av GUI:t. Påbörjad implementation av knapparna

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI {
     private JButton SAVEButton;
@@ -7,4 +9,51 @@ public class GUI {
     private JTextField MESSAGE;
     private JTextArea CONVO;
     private JButton SENDButton;
+    private JPanel MAINPANEL;
+    private JButton HELPButton;
+    private JButton LISTENTOIPButton;
+    private JButton CONNECTONPORTButton;
+
+    public GUI() {
+        SAVEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Under construction!");
+            }
+        });
+        SENDButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Under construction!");
+            }
+        });
+        CONNECTButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ip = JOptionPane.showInputDialog(null, "What IP adress?");
+
+            }
+        });
+        LISTENButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ip = JOptionPane.showInputDialog(null, "What IP adress?");
+            }
+        });
+        HELPButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "ERROR: No help available at this time.");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame GUI = new JFrame("GUI CHAT");
+        GUI.setContentPane(new GUI().MAINPANEL);
+        GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GUI.pack();
+        GUI.setVisible(true);
+
+    }
 }
