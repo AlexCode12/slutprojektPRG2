cc64ec44794a2f1dccf08ba331fe216209451b11
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2020-04-28 09:38:08 +0200

    Nytt GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,32 +1,10 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class GUI extends JFrame {
-
-    public GUI() {
-       JButton nev = new JButton ("New");
-       nev.setBounds(25,100,30,30);
-       JButton listen = new JButton("Listen on port: ");
-       JButton connect = new JButton("Connect");
-        JFrame frame = new JFrame();
-        frame.add(nev);
-        frame.setSize(600, 400);
-       frame.setLayout(null);
-       frame.setVisible(true);
-
-    }
-    private class ActionHandler implements ActionListener {
-        public void actionPerformed(ActionEvent evt){
-            Object source = evt.getSource();
-            /*
-            TÄnkt att behandla alla actions när de uppstår
-             */
-        }
-    }
-
-    public static void main(String[] args) {
-        GUI window = new GUI();
-        window.setVisible(true);
-    }
+public class GUI {
+    private JButton SAVEButton;
+    private JButton LISTENButton;
+    private JButton CONNECTButton;
+    private JTextField MESSAGE;
+    private JTextArea CONVO;
+    private JButton SENDButton;
 }
