81d1c9f3209b57abdaa24dbd12b2ac1462991083
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2020-05-05 09:46:42 +0200

    SAVE knappen öppnar filhantering.

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class GUI {
     private JButton SAVEButton;
@@ -11,20 +12,21 @@ public class GUI {
     private JButton SENDButton;
     private JPanel MAINPANEL;
     private JButton HELPButton;
-    private JButton LISTENTOIPButton;
-    private JButton CONNECTONPORTButton;
+    private JButton SETPORTButton;
+    private JButton EXITButton;
 
     public GUI() {
         SAVEButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Under construction!");
-            }
-        });
-        SENDButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Under construction!");
+                JFileChooser fileDialog = new JFileChooser();
+                File selectedFile;
+                selectedFile = new File("dialog.txt");
+                fileDialog.setSelectedFile(selectedFile);
+                fileDialog.setDialogTitle("Välj vilken fil som ska sparas");
+                int option = fileDialog.showSaveDialog(null);
+                if (option != JFileChooser.APPROVE_OPTION)
+                    return;
             }
         });
         CONNECTButton.addActionListener(new ActionListener() {
@@ -34,18 +36,46 @@ public class GUI {
 
             }
         });
+        SETPORTButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
         LISTENButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String ip = JOptionPane.showInputDialog(null, "What IP adress?");
+
+            }
+        });
+        EXITButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showConfirmDialog(null, "Är du säker på att du vill avsluta?");
+
             }
         });
         HELPButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "ERROR: No help available at this time.");
+                JOptionPane.showMessageDialog(null, "Välkommen till denna chat. Detta gör knapparna:  \n" +
+                        "SAVE FILE: Sparar konversationen till en fil. \n" +
+                        "CONNECT TO IP: Välj vilken IP-adress du vill ansluta till. \n" +
+                        "X: Y \n " +
+                        "Y: Z \n " +
+                        "Z: A \n ");
+
             }
         });
+
+        SENDButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
     }
 
     public static void main(String[] args) {
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
 public class Main {
-    /*
-    Förhoppningsvis blir detta en chat-klon någon dag...
-     */
+
 }
